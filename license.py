5bb91409aa5b52bfd03aefe7e14ddc7f3fa306f1
shadowin4k <163701317+shadowin4k@users.noreply.github.com>
2025-10-25 12:38:29 -0500

    Update license.py

--- license.py
+++ license.py
@@ -104,7 +104,7 @@ def main() -> int:
 
     while True:
         clear_screen()
-        print("- License verification  [+]\n")
+        print("License verification  [+]\n")
 
         key = get_license_key()
         if not key:
